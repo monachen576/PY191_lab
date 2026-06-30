cd08cf729f1d055d874bdb06e00e75f547dd41c5
MC576speedcubing <monachen@bu.edu>
2026-06-30 16:12:05 -0400

    activity 3 done, activity 2 confused

--- assignment_0630.py
+++ assignment_0630.py
@@ -1,3 +1,22 @@
 # Activity 1
 for i in range(10): 
     print(i, end=' ') if i%2==1 else None
+
+
+print("\n")
+
+# Activity 2
+exchange_rate_usd_eur = 0.92
+countries = ["UK", "Italy", "Spain"]
+print(f"The exchange rate for {countries[0]} is {exchange_rate_usd_eur}.")
+# incomplete i dont understand the instructions :(
+
+print()
+
+
+# Activity 3
+for i in range(1, 26): 
+    print(i, end=' ')
+    if (i % 4 == 0): print("Quack", end='')
+    if (i % 6 == 0): print("Moo", end='')
+    print()
